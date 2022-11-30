9f3569a2bae8d917649679f7c70124d5194cd494
cocobutte <cocobutter128@gmail.com>
2022-11-30 17:46:31 +0500

    weather comit

---
+++ weatherapp-with-server/src/App.jsx
@@ -0,0 +1,51 @@
+
+import './App.css';
+
+function App() {
+  
+  return (
+
+    
+
+    <div className='main-div'>
+   
+
+      <div className='sub-div'>
+        <div className='left-div'>
+          <div className='content-div'>
+            <div className='date-div'>
+              <h2>Monday</h2>
+              <p>28 Nov 2022</p>
+
+            </div>
+
+            <div className='city-div'>
+              <p>London</p>
+
+            </div>
+
+            <div className='temp-div'>
+              <h1>10°C</h1>
+
+            </div>
+
+            <div className='weathertype-div'>
+              <p>Thunderstrom</p>
+
+            </div>
+
+          </div>
+      
+          
+
+        </div>
+
+      </div>
+
+    </div>
+  )
+
+  
+}
+
+export default App;
